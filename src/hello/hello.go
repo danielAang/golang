ff4b0d173edc9f79008dbddd894cd5b9ab2ae2bd
daniel sena <danielsena04@gmail.com>
2020-12-02 22:32:48 -0300

    Add site monitoring

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,46 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
+
+const monitorTimes = 3
+const monitorDelay = 5
 
 func main() {
 	showIntro()
-	showMenu()
-	command := readUserInput()
-	switch command { // Do not need to use break inside switch-case statements
-	case 1:
-		fmt.Println("Monitoring applications...")
-		break
-	case 2:
-		fmt.Println("Showing logs")
-	case 0:
-		fmt.Println("Exiting program")
-		os.Exit(0)
-	default:
-		fmt.Println("Invalid input. Exiting program")
-		os.Exit(-1)
-	}
-	/*
-		Prints address
-		fmt.Println(&command)
-	*/
-	/*
-		reflect.TypeOf returns variable type
-		fmt.Println(reflect.TypeOf(command))
-	*/
-	/*
-		If else
-		if command == 1 {
-			fmt.Println("Monitoring applications...")
-		} else if command == 2 {
+	for { // For without parameters, is equivalent to while (true)
+		showMenu()
+		command := readUserInput()
+		switch command { // Do not need to use break inside switch-case statements
+		case 1:
+			startMonitoring()
+			break
+		case 2:
 			fmt.Println("Showing logs")
-		} else if command == 0 {
-			fmt.Println("Exiting program")
-		} else {
-			fmt.Println("Invalid input. Exiting program")
+		case 0:
+			fmt.Println("Exiting program...")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid input. Exiting program...")
+			os.Exit(-1)
 		}
-	*/
+	}
 }
 
 func showIntro() {
@@ -55,6 +42,8 @@ func showIntro() {
 
 func showMenu() {
 	fmt.Println("----------------------------")
+	fmt.Println("|---- SELECT AN OPTION ----|")
+	fmt.Println("----------------------------")
 	fmt.Println("| 1) Monitor applications  |")
 	fmt.Println("| 2) Show logs             |")
 	fmt.Println("| 0) Exit                  |")
@@ -65,4 +54,24 @@ func readUserInput() int {
 	var command int
 	fmt.Scan(&command) //Pointer to command variable
 	return command
+}
+
+func startMonitoring() {
+	fmt.Println("Monitoring applications...")
+	sites := []string{
+		"https://random-status-code.herokuapp.com/",
+		"https://wwww.alura.com.br/",
+		"https://www.caelum.com.br/",
+	}
+	for i := 0; i < monitorTimes; i++ {
+		for _, site := range sites {
+			response, _ := http.Get(site)
+			if 200 == response.StatusCode {
+				fmt.Println("Website", site, "loaded with success:", response.Status)
+			} else {
+				fmt.Println("Failed to load website", site, ":", response.Status)
+			}
+		}
+		time.Sleep(monitorDelay * time.Second)
+	}
 }
